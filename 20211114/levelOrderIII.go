92fd9f5f5fd3c711e1ee7e0c4cd4608a626e98e5
yudongnan <yudongnan@inke.cn>
2021-11-14 15:43:53 +0800

    二叉树的层序遍历III

--- 20211114/levelOrderIII.go
+++ 20211114/levelOrderIII.go
@@ -1,5 +1,44 @@
 package _0211114
 
 /**
- *
+ * 从上到下打印二叉树III：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-iii-lcof/
  */
+
+func levelOrderIII(r *TreeNode) [][]int {
+	rlt := make([][]int, 0)
+	if r == nil {
+		return rlt
+	}
+
+	q := make([]*TreeNode, 1)
+	dump := make(map[*TreeNode]int, 0)
+	q[0] = r
+	dump[r] = 0
+
+	for len(q) != 0 {
+		cur := q[0]
+		q = q[1:]
+
+		if len(rlt) <= dump[cur] {
+			rlt = append(rlt, []int{cur.Val})
+		} else {
+			if dump[cur]%2 == 0 {
+				rlt[dump[cur]] = append(rlt[dump[cur]], cur.Val)
+			} else {
+				rlt[dump[cur]] = append([]int{cur.Val}, rlt[dump[cur]]...)
+			}
+
+		}
+
+		if cur.Left != nil {
+			dump[cur.Left] = dump[cur] + 1
+			q = append(q, cur.Left)
+		}
+
+		if cur.Right != nil {
+			dump[cur.Right] = dump[cur] + 1
+			q = append(q, cur.Right)
+		}
+	}
+	return rlt
+}
--- 20211114/test_test.go
+++ 20211114/test_test.go
@@ -54,3 +54,24 @@ func Test_levelOrderII(t *testing.T) {
 		})
 	}
 }
+
+func Test_levelOrderIII(t *testing.T) {
+	params := []levelOrderIIParam{
+		{
+			name: "1",
+			arg: &TreeNode{Val: 1, Left: &TreeNode{Val: 2, Left: &TreeNode{Val: 4}, Right: &TreeNode{Val: 5}}, Right: &TreeNode{Val: 3, Left: &TreeNode{Val: 6}, Right: &TreeNode{Val: 7}}},
+			want: [][]int{{1}, {3, 2}, {4, 5, 6, 7}},
+		},
+	}
+
+	for _, p := range params {
+		t.Run(p.name, func(t *testing.T) {
+			rlt := levelOrderIII(p.arg)
+			if !reflect.DeepEqual(rlt, p.want) {
+				t.Errorf("rlt: %v not want: %v", rlt, p.want)
+			}
+		})
+
+
+	}
+}
