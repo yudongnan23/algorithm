eff1d0afe86c23a6a3f40f214802a41907d7fbe1
yudongnan <yudongnan@inke.cn>
2021-11-21 19:18:50 +0800

    链表中倒数第k个节点

--- 20211121/getKthFromEnd.go
+++ 20211121/getKthFromEnd.go
@@ -8,6 +8,22 @@ func getKthFromEnd(r *ListNode, k int) *ListNode {
 	if r == nil {
 		return nil
 	}
+	p := r
+	q := r
 
+	// 先让右边指针走k步
+	for i := 0; i < k; i++ {
+		if q == nil {
+			return nil
+		}
+		q = q.Next
+	}
 
+	// 两个指针开始同时走
+	for q != nil {
+		p = p.Next
+		q = q.Next
+	}
+
+	return p
 }
