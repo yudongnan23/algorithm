4ddbd0e1a3e9a87bfd162d5bf33cb2c9d721fd23
yudongnan <yudongnan@inke.cn>
2021-12-19 22:30:58 +0800

    两个栈实现一个队列优化

--- 20211106/tow_stack_queue.go
+++ 20211106/tow_stack_queue.go
@@ -36,3 +36,75 @@ func (c *CQueue) transfer() {
 	c.stack2 = c.stack1
 	c.stack1 = []int{}
 }
+
+// 链表方式实现
+//
+//type CQueue struct {
+//	l1 *linkList
+//	l2 *linkList
+//}
+//
+//type linkList struct {
+//	head *node
+//	tail *node
+//}
+//
+//type node struct {
+//	v    int
+//	next *node
+//	pre  *node
+//}
+//
+//func Constructor() CQueue {
+//	q := CQueue{
+//		l1: &linkList{},
+//		l2: &linkList{},
+//	}
+//	return q
+//}
+//
+//// 队列添加元素
+//// 只添加在第一个栈
+//func (c *CQueue) AppendTail(v int)  {
+//	n := &node{
+//		v: v,
+//	}
+//
+//	// 栈为空
+//	if c.l1.head == nil {
+//		c.l1.head = n
+//		c.l1.tail = n
+//		return
+//	}
+//
+//	n.pre = c.l1.tail
+//	c.l1.tail.next = n
+//	c.l1.tail = c.l1.tail.next
+//	return
+//}
+//
+//func (c *CQueue) DeleteHead() int {
+//	c.move()
+//
+//	if c.l2.head == nil {
+//		return -1
+//	}
+//
+//	v := c.l2.head.v
+//	c.l2.head = c.l2.head.next
+//
+//	return v
+//}
+//
+//func (c *CQueue)move() {
+//	if c.l2.head != nil {
+//		return
+//	}
+//
+//	c.l2.head = c.l1.head
+//	c.l2.tail = c.l1.tail
+//	c.l1.head = nil
+//	c.l1.tail = nil
+//
+//	return
+//}
