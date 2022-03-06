f69f6dff602a43ad13994586278abd8a7b6f5bac
yudongnan <yudongnan@inke.cn>
2022-03-06 21:41:43 +0800

    寻找字符串中第一次只出现一次的字符

---
+++ 2022-03-06/first_uniq_char.go
@@ -0,0 +1,22 @@
+package _022_03_01
+
+// 第一个只出现一次的字符
+// leetcode链接：https://leetcode-cn.com/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
+
+/**
+ * 思路：遍历两遍
+ */
+
+func firstUniqChar(s string) byte {
+	cnt := [26]int{}
+
+	for _, ch := range s {
+		cnt[ch-'a'] += 1
+	}
+	for i, ch := range s {
+		if cnt[ch-'a'] == 1 {
+			return s[i]
+		}
+	}
+	return ' '
+}
