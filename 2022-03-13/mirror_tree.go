a7cb6fc52e0947310ab2ce1ebc55bdd85f9c854a
yudongnan <yudongnan@inke.cn>
2022-03-13 14:40:23 +0800

    树的镜像

--- 2022-03-13/mirror_tree.go
+++ 2022-03-13/mirror_tree.go
@@ -1,2 +1,25 @@
 package _022_03_13
 
+// 树的镜像
+// leetcode链接：https://leetcode-cn.com/problems/er-cha-shu-de-jing-xiang-lcof/
+
+/**
+ * 思路：递归
+ */
+
+func mirrorTree(r *TreeNode) *TreeNode {
+	dfsII(r)
+	return r
+}
+
+func dfsII(r *TreeNode) {
+	if r == nil {
+		return
+	}
+	left := r.Left
+	r.Left = r.Right
+	r.Right = left
+
+	dfsII(r.Left)
+	dfsII(r.Right)
+}
