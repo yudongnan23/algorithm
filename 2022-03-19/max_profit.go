a390e1d3c351bf6a0149096bbac02fa7416252e6
yudongnan <yudongnan@inke.cn>
2022-03-27 17:13:03 +0800

    最大利润调优

--- 2022-03-19/max_profit.go
+++ 2022-03-19/max_profit.go
@@ -7,7 +7,7 @@ package _022_03_19
  * 思路：动态规划
  */
 
-type profit struct {
+type profitElem struct {
 	maxProfit int
 	minIdx    int
 }
@@ -17,33 +17,19 @@ func maxProfit(prices []int) int {
 		return 0
 	}
 
-	dynamic := profit{
+	d := profitElem{
 		maxProfit: 0,
 		minIdx:    0,
 	}
 
 	for i := 1; i < len(prices); i++ {
-		max := Max(prices[i]-prices[dynamic.minIdx], dynamic.maxProfit)
-		if max > dynamic.maxProfit {
-			dynamic.maxProfit = max
+		maxSum := prices[i] - prices[d.minIdx]
+		if maxSum > d.maxProfit {
+			d.maxProfit = maxSum
 		}
-		max = Max(prices[i]-prices[i-1], dynamic.maxProfit)
-		if max > dynamic.maxProfit {
-			dynamic.maxProfit = max
-			dynamic.minIdx = i - 1
+		if prices[i] < prices[d.minIdx] {
+			d.minIdx = i
 		}
-		if prices[i] < prices[dynamic.minIdx] {
-			dynamic.minIdx = i
-		}
-
 	}
-
-	return dynamic.maxProfit
-}
-
-func Max(x, y int) int {
-	if x > y {
-		return x
-	}
-	return y
+	return d.maxProfit
 }
