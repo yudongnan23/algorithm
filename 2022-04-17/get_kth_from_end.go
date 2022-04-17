188ef8f5abd0ad50e86c031384c1043e0337b6ae
yudongnan <yudongnan@inke.cn>
2022-04-17 22:59:38 +0800

    链表中大倒数第k个节点

--- 2022-04-17/get_kth_from_end.go
+++ 2022-04-17/get_kth_from_end.go
@@ -4,5 +4,23 @@ package _022_04_17
 // leetcode链接：https://leetcode-cn.com/problems/lian-biao-zhong-dao-shu-di-kge-jie-dian-lcof/
 
 /**
- * 思路
+ * 思路：双指针
  */
+
+func getKthFromEnd(h *ListNode, k int) *ListNode {
+	r := h
+	for i := 0; r != nil && i < k; i++ {
+		r = r.Next
+	}
+	if r == nil {
+		return nil
+	}
+
+	l := h
+	for r != nil {
+		r = r.Next
+		l = l.Next
+	}
+
+	return l
+}
